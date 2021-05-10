1e6a386888167489140230012f4bf1bac0548591
tien96ng <tien96ng@gmail.com>
2021-05-10 10:01:48 -0700

    Create Court.cs Model

---
+++ Models/Court.cs
@@ -0,0 +1,29 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace open_court.Models
+{
+  public class Court {
+    public int CourtId { get; set; }
+    
+    [Required]
+    [StringLength(255)]
+    public string Name { get; set; }
+
+    [Required]
+    [StringLength(255)]
+    public string Address { get; set; }
+
+    [Required]
+    [Range(0, 50, ErrorMessage = "Number of Hoops can't be a negative number of more than 50.")]
+    public int NumberOfHoops { get; set; }
+
+    [Required]
+    [Range(typeof(bool), "true", "false")]
+    public boolean IsIndoor { get; set; }
+
+    [Required]
+    [Range(typeof(bool), "true", "false")]
+    public boolean IsCovidOpen { get; set; }
+  }
+}
