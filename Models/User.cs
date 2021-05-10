ca4cbbf7f936af233c01d43e78c565bf5d30c31b
tien96ng <tien96ng@gmail.com>
2021-05-10 14:02:51 -0700

    Create User.cs Model and update .gitignore

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Text.Json.Serialization;
+
+namespace open_court.Models
+{
+  public class User
+  {
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public Dictionary<int, string> Favorites { get; set; }
+    public Dictionary<int, Dictionary<int, string>> Interactions { get; set; } 
+    
+    [JsonIgnore]
+    public string Password { get; set; }
+  }
+}
