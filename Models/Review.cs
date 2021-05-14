3ad3d6011e23f34b98a938487e2d2cc8afbb7344
tien96ng <tien96ng@gmail.com>
2021-05-14 16:17:52 -0700

    Create Review.cs Model to house the 1 to many relationship

---
+++ Models/Review.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace open_court.Models
+{
+  public class Review
+  {
+    public int ReviewId { get; set; }
+    public DateTime PostDate { get; set; }
+    public string ReviewText { get; set; }
+    public virtual Court Court { get; set; }
+  }
+}
