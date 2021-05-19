9364fd77cc4aa04651a997835f61390438d49f22
tien96ng <tien96ng@gmail.com>
2021-05-18 21:22:42 -0700

    Created lefthand court list section before form

--- ClientApp/src/components/CourtList.js
+++ ClientApp/src/components/CourtList.js
@@ -1,20 +1,24 @@
 import React, { useContext, useState, useEffect } from "react";
-import { render } from "react-dom";
+import { Row, Col } from "reactstrap"
 import { Context } from "../context/appContext";
 
 export default function CourtList() {
   const { courts, search, setSearchResults, searchResults } = useContext(Context);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   const [filters, setFilters] = useState({
     sort: "name",
     isIndoor: false,
     isCovidOpen: true,
-    state: "America"
+    state: "America",
+    totalReviews: 0
   });
   
   const renderStateName = () => {
+    let count = 0;
     let filteredCourts = courts.filter(c => c.address.includes(search));
+    filteredCourts.forEach(c => count += c.totalRatingCount);
     setSearchResults(filteredCourts);
+    setFilters({...filters, totalReviews: count});
   }
 
   useEffect(() => {
@@ -24,7 +28,7 @@ export default function CourtList() {
       renderStateName();
     }, 1000);
     return () => {
-      console.log("Clean up");
+      console.log("Clean up SearchResults.");
     }
   }, [loading])
 
@@ -33,21 +37,29 @@ export default function CourtList() {
     if(searchResults.length > 0) {
       setFilters({...filters, state: searchResults[0].address.split(", ")[2]});
     }
+    return () => {
+      console.log("Clean up Filters.")
+    }
   }, [searchResults])
 
   return(
     <>
-    <h1>Hello World</h1>
       <div className="row">
         <div className="column-1">
           <div className="left-column">
-            <h1>Courts in {filters.state}</h1>
-            
+            <Row><h1 className="page-subtitle">Courts in {filters.state}</h1></Row>
+            <Row><h6>{filters.totalReviews} hooper reviews</h6></Row>
           </div>
         </div>
         <div className="column-3">
           <div className="right-column">
-            {loading ? <h1>No Courts Found</h1> : <h1> {searchResults.length} </h1>}
+            {
+              loading ? 
+              <h2>No Courts Found</h2> : 
+              <h2>
+                <span className="orange-text">{searchResults.length} courts</span> match your search 
+              </h2>
+            }
           </div>
         </div>
       </div>
