6021df7ed7000a0af9565150f7c088c2fa1b9557
tien96ng <tien96ng@gmail.com>
2021-05-18 19:02:54 -0700

    Update NavMenu and removed Counter component

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -10,18 +10,12 @@ export default function NavMenu() {
 
   return(
     <header>
-      <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow bgc-white" light fixed>
+      <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow bgc-white" light fixed="top">
         <Container>
           <NavbarBrand tag={Link} to="/" id="homeBtn-text">Open Court</NavbarBrand>
           <NavbarToggler onClick={toggleNavBar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!collapsed} navbar>
             <ul className="navbar-nav flex-grow font-weight-bold">
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-              </NavItem>
               <NavItem>
                 <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
               </NavItem>
